IMP: remove numerical _acceleration keyword from FlowModel, add some tests + prepare for variable timesteps in flow and transport (the gradient is not correct for now).

pyrtid/forward/flow_solver.py
@@ -1,13 +1,13 @@
 """Provide a reactive transport solver."""
 from __future__ import annotations
 
-from typing import Optional, Tuple
+from typing import Tuple
 
 import numpy as np
-from scipy.sparse import csc_matrix, lil_matrix
-from scipy.sparse.linalg import LinearOperator, gmres
+from scipy.sparse import lil_matrix
+from scipy.sparse.linalg import gmres
 
-from pyrtid.utils import harmonic_mean
+from pyrtid.utils import get_super_lu_preconditioner, harmonic_mean
 from pyrtid.utils.types import NDArrayFloat
 
 from .models import FlowModel, Geometry, TimeParameters, get_owner_neigh_indices
@@ -15,7 +15,7 @@ from .models import FlowModel, Geometry, TimeParameters, get_owner_neigh_indices
 
 def make_stationary_flow_matrices(
     geometry: Geometry, fl_model: FlowModel
-) -> csc_matrix:
+) -> lil_matrix:
     """
     Make matrices for the transient flow.
 
@@ -91,12 +91,12 @@ def make_stationary_flow_matrices(
     # Take constant head into account
     q_next[fl_model.cst_head_nn, fl_model.cst_head_nn] = 1.0
 
-    return q_next.tocsc()
+    return q_next
 
 
 def make_transient_flow_matrices(
     geometry: Geometry, fl_model: FlowModel, time_params: TimeParameters
-) -> Tuple[csc_matrix, csc_matrix]:
+) -> Tuple[lil_matrix, lil_matrix]:
     """
     Make matrices for the transient flow.
 
@@ -258,17 +258,12 @@ def make_transient_flow_matrices(
             / fl_model.storage_coefficient
         )
 
-    # Add 1/dt for the left term contribution
-    q_next.setdiag(q_next.diagonal() + 1 / time_params.dt)
-    q_prev.setdiag(q_prev.diagonal() + 1 / time_params.dt)
-
-    return q_next.tocsc(), q_prev.tocsc()
+    return q_next, q_prev
 
 
 def solve_flow_stationary(
     geometry: Geometry,
     fl_model: FlowModel,
-    preconditioner: Optional[LinearOperator],
     time_index: int,
 ) -> int:
     """
@@ -282,12 +277,15 @@ def solve_flow_stationary(
         fl_model.cst_head_nn
     ]
 
+    # LU preconditioner
+    preconditioner = get_super_lu_preconditioner(fl_model.q_next.tocsc())
+
     # Add the source terms
     tmp += fl_model.sources[:, :, time_index].flatten(order="F")
 
     # Solve Ax = b with A sparse using LU preconditioner
     res, exit_code = gmres(
-        fl_model.q_next, tmp, M=preconditioner, atol=fl_model.tolerance
+        fl_model.q_next.tocsc(), tmp, M=preconditioner, atol=fl_model.tolerance
     )
     fl_model.head[:, :, time_index] = res.reshape(geometry.ny, geometry.nx).T
 
@@ -449,7 +447,6 @@ def update_u_darcy_div(
 def solve_flow_transient_semi_implicit(
     geometry: Geometry,
     fl_model: FlowModel,
-    preconditioner: Optional[LinearOperator],
     time_params: TimeParameters,
     time_index: int,
 ) -> int:
@@ -458,8 +455,22 @@ def solve_flow_transient_semi_implicit(
 
     dh/dt = div K grad h + ...
     """
+    _q_next = fl_model.q_next.copy()
+    _q_prev = fl_model.q_prev.copy()
+
+    # Add 1/dt for the left term contribution (note: the timestep is variable)
+    _q_next.setdiag(_q_next.diagonal() + 1 / time_params.dt)
+    _q_prev.setdiag(_q_prev.diagonal() + 1 / time_params.dt)
+
+    # csc format for efficiency
+    _q_next = _q_next.tocsc()
+    _q_prev = _q_prev.tocsc()
+
+    # Get LU preconditioner
+    preconditioner = get_super_lu_preconditioner(_q_next)
+
     # Multiply prev matrix by prev vector
-    tmp = fl_model.q_prev.dot(fl_model.head[:, :, time_index - 1].flatten(order="F"))
+    tmp = _q_prev.dot(fl_model.head[:, :, time_index - 1].flatten(order="F"))
 
     # Add the source terms
     sources = (
@@ -470,19 +481,9 @@ def solve_flow_transient_semi_implicit(
 
     tmp += sources
 
-    # Use the last state as initial guess for the solver.
-    # Also takes the sources/sinks into account
-    if fl_model.is_numerical_acceleration:
-        x0 = (
-            fl_model.head[:, :, time_index - 1].flatten(order="F")
-            + sources * time_params.dt
-        )
-    else:
-        x0 = None
-
     # Solve Ax = b with A sparse using LU preconditioner
     res, exit_code = gmres(
-        fl_model.q_next, tmp, x0=x0, M=preconditioner, atol=fl_model.tolerance
+        _q_next, tmp, x0=None, M=preconditioner, atol=fl_model.tolerance
     )
     fl_model.head[:, :, time_index] = res.reshape(geometry.ny, geometry.nx).T
 

pyrtid/forward/models.py
@@ -13,7 +13,7 @@ from dataclasses import dataclass
 from typing import List, Optional, Sequence, Tuple, Union
 
 import numpy as np
-from scipy.sparse import csc_matrix
+from scipy.sparse import csc_matrix, lil_matrix
 
 from pyrtid.utils import StrEnum, node_number_to_indices, span_to_node_numbers_2d
 from pyrtid.utils.types import (
@@ -59,17 +59,37 @@ class TimeParameters:
     ) -> None:
         """Initialize the instance."""
         self.nt: int = int(nt)
-        self.dt_init: float = dt_init
-        self.dt: float = dt_init
+
+        # First pass on the min/max
+        if dt_min is not None:
+            _dt_min: float = dt_min
+        else:
+            _dt_min = dt_init
+        if dt_max is not None:
+            _dt_max: float = dt_max
+        else:
+            _dt_max = dt_init
+
+        _dt_init = max(min(_dt_max, dt_init), _dt_min, dt_init)
+
+        # Second pass on the min/max
         if dt_min is not None:
             self.dt_min: float = dt_min
         else:
-            self.dt_min = self.dt
+            self.dt_min = _dt_init
         if dt_max is not None:
             self.dt_max: float = dt_max
         else:
-            self.dt_max = self.dt
-        self.ldt: List[float] = []
+            self.dt_max = _dt_init
+
+        # Check dt_min and dt_max consistency
+        if self.dt_min > self.dt_max:
+            raise ValueError(f"dt_min ({self.dt_min}) is above dt_max ({self.dt_max})!")
+
+        # Apply bounds
+        self.dt_init: float = _dt_init
+        self.dt = _dt_init
+        self.ldt: List[float] = [self.dt]
 
     @property
     def duration(self) -> float:
@@ -79,7 +99,7 @@ class TimeParameters:
     def reset_dt(self) -> None:
         """Empty the list of timesteps and set dt to its initial value."""
         self.dt = self.dt_init
-        self.ldt = []
+        self.ldt = [self.dt_init]
 
     def update_dt(self, n_iter: int) -> None:
         """
@@ -90,6 +110,9 @@ class TimeParameters:
         n_iter: int
             Number of iterations required to solve the current timestep.
         """
+        # Save the previous timestep
+        self.ldt.append(self.dt)
+
         if n_iter < 20:
             # increase dt by 5%
             self.dt *= 1.05
@@ -101,7 +124,6 @@ class TimeParameters:
             self.dt = self.dt_min
         if self.dt > self.dt_max:
             self.dt = self.dt_max
-        self.ldt.append(self.dt)
 
 
 class FlowRegime(StrEnum):
@@ -138,10 +160,6 @@ class FlowParameters:
         enabled. The default is the z axis.
     tolerance: float, optional
         The tolerance on the flow. The default is 1e-8.
-    is_numerical_acceleration: bool, optional
-        Whether to use the head values at the previous iteration as an initial guess
-        for gmres (x0) rather than using a random vector. This should increase the
-        speed of resolution. The default is False.
     """
 
     def __init__(
@@ -153,7 +171,6 @@ class FlowParameters:
         is_gravity: bool = False,
         vertical_axis: VerticalAxis = VerticalAxis.DZ,
         tolerance: float = 1e-8,
-        is_numerical_acceleration: bool = False,
     ) -> None:
         """Initialize the instance."""
         self.permeability: float = permeability
@@ -163,7 +180,6 @@ class FlowParameters:
         self.is_gravity: bool = is_gravity
         self.vertical_axis: VerticalAxis = vertical_axis
         self.tolerance: float = tolerance
-        self.is_numerical_acceleration: bool = is_numerical_acceleration
 
 
 class TransportParameters:
@@ -457,7 +473,6 @@ class FlowModel:
         "q_prev",
         "q_next",
         "tolerance",
-        "is_numerical_acceleration",
     ]
 
     def __init__(
@@ -487,11 +502,10 @@ class FlowModel:
         self.sources = np.zeros(
             (geometry.nx, geometry.ny, time_params.nt + 1), dtype=np.float64
         )
-        self.q_prev = csc_matrix(geometry.nx * geometry.ny)
-        self.q_next = csc_matrix(geometry.nx * geometry.ny)
+        self.q_prev = lil_matrix(geometry.nx * geometry.ny)
+        self.q_next = lil_matrix(geometry.nx * geometry.ny)
         self.cst_head_nn: NDArrayInt = np.array([], dtype=np.int32)
         self.tolerance = fl_params.tolerance
-        self.is_numerical_acceleration = fl_params.is_numerical_acceleration
         self.vertical_axis = fl_params.vertical_axis
         self.vertical_mesh_size = {
             VerticalAxis.DX: geometry.dx,
@@ -676,8 +690,8 @@ class TransportModel:
             (geometry.nx, geometry.ny, time_params.nt + 1), dtype=np.float64
         )
         # q_prev is composed of q_prev_diffusion + advection term
-        self.q_prev_diffusion = csc_matrix(geometry.nx * geometry.ny)
-        self.q_next_diffusion = csc_matrix(geometry.nx * geometry.ny)
+        self.q_prev_diffusion = lil_matrix(geometry.nx * geometry.ny)
+        self.q_next_diffusion = lil_matrix(geometry.nx * geometry.ny)
         self.q_prev = csc_matrix(geometry.nx * geometry.ny)
         self.q_next = csc_matrix(geometry.nx * geometry.ny)
         self.cst_conc_indices: NDArrayInt = np.array([], dtype=np.int32)

pyrtid/forward/transport_solver.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import Tuple
 
 import numpy as np
-from scipy.sparse import csc_matrix, lil_matrix
+from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import gmres
 
 from pyrtid.utils import harmonic_mean
@@ -22,7 +22,7 @@ from .models import (
 
 def make_transport_matrices_diffusion_only(
     geometry: Geometry, tr_model: TransportModel, time_params: TimeParameters
-) -> Tuple[csc_matrix, csc_matrix]:
+) -> Tuple[lil_matrix, lil_matrix]:
     """
     Make matrices for the transport.
 
@@ -152,19 +152,23 @@ def make_transport_matrices_diffusion_only(
             / geometry.dy**2
         )
 
-    # Add 1/dt for the left term contribution
-    q_next.setdiag(q_next.diagonal() + tr_model.porosity.flatten("F") / time_params.dt)
-    q_prev.setdiag(q_prev.diagonal() + tr_model.porosity.flatten("F") / time_params.dt)
-
-    return q_next.tocsc(), q_prev.tocsc()
+    return q_next, q_prev
 
 
 def _add_advection_to_transport_matrices(
-    geometry: Geometry, fl_model: FlowModel, tr_model: TransportModel, time_index: int
+    geometry: Geometry,
+    fl_model: FlowModel,
+    tr_model: TransportModel,
+    time_params: TimeParameters,
+    time_index: int,
 ) -> None:
-    crank_adv = tr_model.crank_nicolson_advection
-    q_next = tr_model.q_next_diffusion.copy().tolil()
-    q_prev = tr_model.q_prev_diffusion.copy().tolil()
+    crank_adv: float = tr_model.crank_nicolson_advection
+    q_next: lil_matrix = tr_model.q_next_diffusion.copy()
+    q_prev: lil_matrix = tr_model.q_prev_diffusion.copy()
+
+    # Add 1/dt for the left term contribution
+    q_next.setdiag(q_next.diagonal() + tr_model.porosity.flatten("F") / time_params.dt)
+    q_prev.setdiag(q_prev.diagonal() + tr_model.porosity.flatten("F") / time_params.dt)
 
     # X contribution
     if geometry.nx >= 2:
@@ -444,7 +448,9 @@ def solve_transport_semi_implicit(
     """Compute the conc and grade fields by solving the flow problem."""
 
     # Update q_next and q_prev with the advection term (must be copied)
-    _add_advection_to_transport_matrices(geometry, fl_model, tr_model, time_index)
+    _add_advection_to_transport_matrices(
+        geometry, fl_model, tr_model, time_params, time_index
+    )
 
     # Multiply prev matrix by prev vector
     tmp = tr_model.q_prev.dot(tr_model.conc[:, :, time_index - 1].flatten(order="F"))
@@ -458,13 +464,14 @@ def solve_transport_semi_implicit(
 
     # Use the last state as initial guess for the solver.
     # Also takes the sources/sinks into account
-    if tr_model.is_numerical_acceleration:
-        x0 = (
-            tr_model.conc[:, :, time_index - 1].flatten(order="F")
-            + _get_transport_source_term(tr_model, time_index) * time_params.dt
-        )
-    else:
-        x0 = None
+    # TODO: this is for the chemical source term
+    # if tr_model.is_numerical_acceleration:
+    #     x0 = (
+    #         tr_model.conc[:, :, time_index - 1].flatten(order="F")
+    #         + _get_transport_source_term(tr_model, time_index) * time_params.dt
+    #     )
+    # else:
+    x0 = None
 
     # Solve Ax = b with A sparse using LU preconditioner
     res, exit_code = gmres(

pyrtid/inverse/adjoint/aflow_solver.py
@@ -1,11 +1,11 @@
 """Provide an adjoint solver and model."""
 from __future__ import annotations
 
-from typing import Optional, Tuple
+from typing import Tuple
 
 import numpy as np
-from scipy.sparse import csc_matrix, lil_matrix
-from scipy.sparse.linalg import LinearOperator, gmres
+from scipy.sparse import lil_matrix
+from scipy.sparse.linalg import gmres
 
 from pyrtid.forward.models import (  # ConstantHead,; ZeroConcGradient,
     FlowModel,
@@ -15,13 +15,13 @@ from pyrtid.forward.models import (  # ConstantHead,; ZeroConcGradient,
     get_owner_neigh_indices,
 )
 from pyrtid.inverse.adjoint.amodels import AdjointFlowModel, AdjointTransportModel
-from pyrtid.utils import harmonic_mean
+from pyrtid.utils import get_super_lu_preconditioner, harmonic_mean
 from pyrtid.utils.types import NDArrayFloat
 
 
 def make_stationary_adj_flow_matrices(
     geometry: Geometry, fl_model: FlowModel, time_params: TimeParameters
-) -> Tuple[csc_matrix, csc_matrix]:
+) -> Tuple[lil_matrix, lil_matrix]:
     """
     Make matrices for the transient flow.
 
@@ -127,15 +127,15 @@ def make_stationary_adj_flow_matrices(
 
     # Add 1/dt for the left term contribution
     q_prev.setdiag(
-        q_prev.diagonal() + 1 / time_params.dt * fl_model.storage_coefficient
+        q_prev.diagonal() + 1 / time_params.ldt[0] * fl_model.storage_coefficient
     )
 
-    return q_next.tocsc(), q_prev.tocsc()
+    return q_next, q_prev
 
 
 def make_transient_adj_flow_matrices(
     geometry: Geometry, fl_model: FlowModel, time_params: TimeParameters
-) -> Tuple[csc_matrix, csc_matrix]:
+) -> Tuple[lil_matrix, lil_matrix]:
     """
     Make matrices for the transient flow.
 
@@ -299,18 +299,13 @@ def make_transient_adj_flow_matrices(
             / fl_model.storage_coefficient
         )
 
-    # Add 1/dt for the left term contribution
-    q_next.setdiag(q_next.diagonal() + 1 / time_params.dt)
-    q_prev.setdiag(q_prev.diagonal() + 1 / time_params.dt)
-
-    return q_next.tocsc(), q_prev.tocsc()
+    return q_next, q_prev
 
 
 def solve_adj_flow_stationary(
     geometry: Geometry,
     fl_model: FlowModel,
     a_fl_model: AdjointFlowModel,
-    preconditioner: Optional[LinearOperator],
     time_index: int,
 ) -> int:
     """
@@ -327,13 +322,15 @@ def solve_adj_flow_stationary(
     # Add the source terms from head observations
     tmp -= a_fl_model.a_sources[:, :, time_index].ravel("F") / geometry.mesh_area
 
+    preconditioner = get_super_lu_preconditioner(a_fl_model.q_next.tocsc())
+
     # Add the source terms from mob observations (adjoint transport)
     # tmp += _get_adjoint_transport_src_terms(
     #     geometry, fl_model, a_fl_model, time_index, False
     # )
 
     # Solve Ax = b with A sparse using LU preconditioner
-    res, exit_code = gmres(a_fl_model.q_next, tmp, M=preconditioner, atol=1e-15)
+    res, exit_code = gmres(a_fl_model.q_next.tocsc(), tmp, M=preconditioner, atol=1e-15)
     # Note: we solve
     a_fl_model.a_head[:, :, time_index] = res.reshape(geometry.ny, geometry.nx).T
 
@@ -469,7 +466,7 @@ def solve_adj_flow_transient_semi_implicit(
     geometry: Geometry,
     fl_model: FlowModel,
     a_fl_model: AdjointFlowModel,
-    preconditioner: Optional[LinearOperator],
+    time_params: TimeParameters,
     time_index: int,
     is_mob_obs: bool,
 ) -> int:
@@ -478,8 +475,19 @@ def solve_adj_flow_transient_semi_implicit(
 
     dh/dt = div K grad h + ...
     """
-    _q_prev = a_fl_model.q_prev
-    _q_next = a_fl_model.q_next
+    _q_prev = a_fl_model.q_prev.copy()
+    _q_next = a_fl_model.q_next.copy()
+
+    # Add 1/dt for the left term contribution
+    _q_next.setdiag(_q_next.diagonal() + 1 / time_params.ldt[time_index - 2])
+    _q_prev.setdiag(_q_prev.diagonal() + 1 / time_params.ldt[time_index - 1])
+
+    # convert to csc format for efficiency
+    _q_next = _q_next.tocsc()
+    _q_prev = _q_prev.tocsc()
+
+    # LU preconditioner
+    preconditioner = get_super_lu_preconditioner(_q_next)
 
     # Handle the first time step in the adjoint (= last timestep in the forward)
     # if time_index + 1 != a_fl_model.a_sources.shape[-1]:

pyrtid/inverse/adjoint/amodels.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import numpy as np
-from scipy.sparse import csc_matrix
+from scipy.sparse import csc_matrix, lil_matrix
 
 from pyrtid.forward.models import (  # ConstantHead,; ZeroConcGradient,
     ForwardModel,
@@ -41,8 +41,8 @@ class AdjointFlowModel:
         self.a_sources = np.zeros(
             (geometry.nx, geometry.ny, time_params.nt + 1), dtype=np.float64
         )
-        self.q_prev = csc_matrix(geometry.nx * geometry.ny)
-        self.q_next = csc_matrix(geometry.nx * geometry.ny)
+        self.q_prev = lil_matrix(geometry.nx * geometry.ny)
+        self.q_next = lil_matrix(geometry.nx * geometry.ny)
 
 
 class AdjointTransportModel: