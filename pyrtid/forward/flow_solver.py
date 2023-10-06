FIX: gradient perm stationary

pyrtid/forward/flow_solver.py
@@ -7,10 +7,7 @@ import numpy as np
 from scipy.sparse import lil_array
 from scipy.sparse.linalg import gmres
 
-from pyrtid.utils import (
-    get_super_lu_preconditioner,
-    harmonic_mean,
-)
+from pyrtid.utils import get_super_lu_preconditioner, harmonic_mean
 from pyrtid.utils.types import NDArrayFloat
 
 from .models import (
@@ -44,6 +41,8 @@ def make_stationary_flow_matrices(geometry: Geometry, fl_model: FlowModel) -> li
         )
         kmean = kmean.flatten(order="F")
 
+        tmp = geometry.dy / geometry.dx / geometry.mesh_volume
+
         # Forward scheme:
         idc_owner, idc_neigh = get_owner_neigh_indices(
             geometry,
@@ -51,8 +50,6 @@ def make_stationary_flow_matrices(geometry: Geometry, fl_model: FlowModel) -> li
             (slice(1, geometry.nx), slice(None)),
             owner_indices_to_keep=fl_model.free_head_nn,
         )
-
-        tmp = geometry.dy / geometry.dx / geometry.mesh_volume
 
         q_next[idc_owner, idc_neigh] -= kmean[idc_owner] * tmp  # type: ignore
         q_next[idc_owner, idc_owner] += kmean[idc_owner] * tmp  # type: ignore
@@ -76,6 +73,8 @@ def make_stationary_flow_matrices(geometry: Geometry, fl_model: FlowModel) -> li
         )
         kmean = kmean.flatten(order="F")
 
+        tmp = geometry.dx / geometry.dy / geometry.mesh_volume
+
         # Forward scheme:
         idc_owner, idc_neigh = get_owner_neigh_indices(
             geometry,
@@ -83,8 +82,6 @@ def make_stationary_flow_matrices(geometry: Geometry, fl_model: FlowModel) -> li
             (slice(None), slice(1, geometry.ny)),
             owner_indices_to_keep=fl_model.free_head_nn,
         )
-
-        tmp = geometry.dx / geometry.dy / geometry.mesh_volume
 
         q_next[idc_owner, idc_neigh] -= kmean[idc_owner] * tmp  # type: ignore
         q_next[idc_owner, idc_owner] += kmean[idc_owner] * tmp  # type: ignore
@@ -122,16 +119,17 @@ def make_transient_flow_matrices(
     q_prev = lil_array((dim, dim), dtype=np.float64)
     q_next = lil_array((dim, dim), dtype=np.float64)
 
-    # X contribution
-    kmean: NDArrayFloat = np.zeros((geometry.nx, geometry.ny), dtype=np.float64)
-    kmean[:-1, :] = harmonic_mean(
-        fl_model.permeability[:-1, :], fl_model.permeability[1:, :]
-    )
-    kmean = kmean.flatten(order="F")
     stocoeff = fl_model.storage_coefficient.ravel("F")
 
-    # Forward scheme:
+    # X contribution
     if geometry.nx >= 2:
+        kmean: NDArrayFloat = np.zeros((geometry.nx, geometry.ny), dtype=np.float64)
+        kmean[:-1, :] = harmonic_mean(
+            fl_model.permeability[:-1, :], fl_model.permeability[1:, :]
+        )
+        kmean = kmean.flatten(order="F")
+
+        # Forward scheme:
         idc_owner, idc_neigh = get_owner_neigh_indices(
             geometry,
             (slice(0, geometry.nx - 1), slice(None)),
@@ -161,6 +159,8 @@ def make_transient_flow_matrices(
             (slice(0, geometry.nx - 1), slice(None)),
             owner_indices_to_keep=fl_model.free_head_nn,
         )
+
+        tmp = geometry.dy / geometry.dx / geometry.mesh_volume / stocoeff[idc_owner]
 
         q_next[idc_owner, idc_neigh] -= (
             fl_model.crank_nicolson * kmean[idc_neigh] * tmp
@@ -213,6 +213,8 @@ def make_transient_flow_matrices(
             (slice(None), slice(0, geometry.ny - 1)),
             owner_indices_to_keep=fl_model.free_head_nn,
         )
+
+        tmp = geometry.dx / geometry.dy / geometry.mesh_volume / stocoeff[idc_owner]
 
         q_next[idc_owner, idc_neigh] -= (
             fl_model.crank_nicolson * kmean[idc_neigh] * tmp

pyrtid/inverse/adjoint/aflow_solver.py
@@ -41,10 +41,6 @@ def make_initial_adj_flow_matrices(
     q_next = lil_array((dim, dim), dtype=np.float64)
     stocoeff = fl_model.storage_coefficient.ravel("F")
 
-    # # X contribution
-    # kmean = harmonic_mean(fl_model.permeability[:-1, :], fl_model.permeability[1:, :])
-    tmp = geometry.dy / geometry.dx / geometry.mesh_volume
-
     # 1) X contribution
     if geometry.nx >= 2:
         kmean: NDArrayFloat = np.zeros((geometry.nx, geometry.ny), dtype=np.float64)
@@ -52,6 +48,8 @@ def make_initial_adj_flow_matrices(
             fl_model.permeability[:-1, :], fl_model.permeability[1:, :]
         )
         kmean = kmean.flatten(order="F")
+
+        tmp = geometry.dy / geometry.dx / geometry.mesh_volume
 
         # 1.1) Forward scheme:
 
@@ -121,9 +119,10 @@ def make_initial_adj_flow_matrices(
             neigh_indices_to_keep=fl_model.free_head_nn,
         )
 
-        q_next[idc_owner, idc_neigh] -= (
-            kmean[idc_neigh] * tmp / stocoeff[idc_owner]  # type: ignore
-        )
+        if fl_model.regime == FlowRegime.STATIONARY:
+            q_next[idc_owner, idc_neigh] -= (
+                kmean[idc_neigh] * tmp / stocoeff[idc_owner]  # type: ignore
+            )
 
         q_prev[idc_owner, idc_neigh] += (
             (1.0 - fl_model.crank_nicolson)
@@ -134,6 +133,7 @@ def make_initial_adj_flow_matrices(
 
     # 2) Y contribution
     if geometry.ny >= 2:
+        print("yoooo")
         kmean: NDArrayFloat = np.zeros((geometry.nx, geometry.ny), dtype=np.float64)
         kmean[:, :-1] = harmonic_mean(
             fl_model.permeability[:, :-1], fl_model.permeability[:, 1:]
@@ -267,11 +267,10 @@ def make_transient_adj_flow_matrices(
     q_next = lil_array((dim, dim), dtype=np.float64)
     stocoeff = fl_model.storage_coefficient.ravel("F")
 
-    # # X contribution
-    _tmp = geometry.dy / geometry.dx / geometry.mesh_volume
-
     # 1) X contribution
     if geometry.nx >= 2:
+        _tmp = geometry.dy / geometry.dx / geometry.mesh_volume
+
         kmean: NDArrayFloat = np.zeros((geometry.nx, geometry.ny), dtype=np.float64)
         kmean[:-1, :] = harmonic_mean(
             fl_model.permeability[:-1, :], fl_model.permeability[1:, :]
@@ -361,7 +360,7 @@ def make_transient_adj_flow_matrices(
         kmean = kmean.flatten(order="F")
 
         # 2.1) Forward scheme:
-        tmp = geometry.dx / geometry.dy / geometry.mesh_volume
+        _tmp = geometry.dx / geometry.dy / geometry.mesh_volume
 
         # 2.1.1) For free head nodes only
         idc_owner, idc_neigh = get_owner_neigh_indices(

pyrtid/inverse/adjoint/gradients.py
@@ -295,58 +295,70 @@ def _get_perm_gradient_from_diffusivity_eq(
         free_head_indices[0], free_head_indices[1], :
     ]
 
+    grad = np.zeros(shape)
+
     # Consider the x axis
-    # Forward scheme
-    dhead_fx = np.zeros(shape)
-    dhead_fx[:-1, :, 1:] += (
-        crank_flow * (head[1:, :, 1:] - head[:-1, :, 1:])
-        + (1.0 - crank_flow) * (head[1:, :, :-1] - head[:-1, :, :-1])
-    ) * dxi_harmonic_mean(permeability[:-1, :], permeability[1:, :])[:, :, np.newaxis]
+    if shape[0] > 1:
+        # Forward scheme
+        dhead_fx = np.zeros(shape)
+        dhead_fx[:-1, :, 1:] += (
+            crank_flow * (head[1:, :, 1:] - head[:-1, :, 1:])
+            + (1.0 - crank_flow) * (head[1:, :, :-1] - head[:-1, :, :-1])
+        ) * dxi_harmonic_mean(permeability[:-1, :], permeability[1:, :])[
+            :, :, np.newaxis
+        ]
 
-    dahead_fx = np.zeros(shape)
-    dahead_fx[:-1, :, :] += ma_ahead[1:, :, :] - ma_ahead[:-1, :, :]
+        dahead_fx = np.zeros(shape)
+        dahead_fx[:-1, :, :] += ma_ahead[1:, :, :] - ma_ahead[:-1, :, :]
 
-    # Handle the stationary case
-    if fwd_model.fl_model.regime == FlowRegime.STATIONARY:
-        dhead_fx[:-1, :, :1] = (head[1:, :, :1] - head[:-1, :, :1]) * dxi_harmonic_mean(
-            permeability[:-1, :], permeability[1:, :]
-        )[:, :, np.newaxis]
+        # Handle the stationary case
+        if fwd_model.fl_model.regime == FlowRegime.STATIONARY:
+            dhead_fx[:-1, :, :1] = (
+                head[1:, :, :1] - head[:-1, :, :1]
+            ) * dxi_harmonic_mean(permeability[:-1, :], permeability[1:, :])[
+                :, :, np.newaxis
+            ]
 
-    # Bheadkward scheme
-    dhead_bx = np.zeros(shape)
-    dhead_bx[1:, :, 1:] += (
-        crank_flow * (head[:-1, :, 1:] - head[1:, :, 1:])
-        + (1.0 - crank_flow) * (head[:-1, :, :-1] - head[1:, :, :-1])
-    ) * dxi_harmonic_mean(permeability[1:, :], permeability[:-1, :])[:, :, np.newaxis]
-    dahead_bx = np.zeros(shape)
+        # Bheadkward scheme
+        dhead_bx = np.zeros(shape)
+        dhead_bx[1:, :, 1:] += (
+            crank_flow * (head[:-1, :, 1:] - head[1:, :, 1:])
+            + (1.0 - crank_flow) * (head[:-1, :, :-1] - head[1:, :, :-1])
+        ) * dxi_harmonic_mean(permeability[1:, :], permeability[:-1, :])[
+            :, :, np.newaxis
+        ]
+        dahead_bx = np.zeros(shape)
 
-    dahead_bx[1:, :, :] += ma_ahead[:-1, :, :] - ma_ahead[1:, :, :]
+        dahead_bx[1:, :, :] += ma_ahead[:-1, :, :] - ma_ahead[1:, :, :]
 
-    # Handle the stationary case
-    if fwd_model.fl_model.regime == FlowRegime.STATIONARY:
-        dhead_bx[1:, :, :1] = (head[:-1, :, :1] - head[1:, :, :1]) * dxi_harmonic_mean(
-            permeability[1:, :], permeability[:-1, :]
-        )[:, :, np.newaxis]
+        # Handle the stationary case
+        if fwd_model.fl_model.regime == FlowRegime.STATIONARY:
+            dhead_bx[1:, :, :1] = (
+                head[:-1, :, :1] - head[1:, :, :1]
+            ) * dxi_harmonic_mean(permeability[1:, :], permeability[:-1, :])[
+                :, :, np.newaxis
+            ]
 
-    # Gather the two schemes
-    grad = (
-        (dhead_fx * dahead_fx + dhead_bx * dahead_bx)
-        * fwd_model.geometry.dy
-        / fwd_model.geometry.dx
-    )
+        # Gather the two schemes
+        grad += (
+            (dhead_fx * dahead_fx + dhead_bx * dahead_bx)
+            * fwd_model.geometry.dy
+            / fwd_model.geometry.dx
+        )
 
     # Consider the y axis for 2D cases
-    if shape[1] != 1:
+    if shape[1] > 1:
         # Forward scheme
         dhead_fy = np.zeros(shape)
-        dhead_fy[:, :-1, :-1] += (
+        dhead_fy[:, :-1, 1:] += (
             crank_flow * (head[:, 1:, 1:] - head[:, :-1, 1:])
             + (1.0 - crank_flow) * (head[:, 1:, :-1] - head[:, :-1, :-1])
         ) * dxi_harmonic_mean(permeability[:, :-1], permeability[:, 1:])[
             :, :, np.newaxis
         ]
         dahead_fy = np.zeros(shape)
-        dahead_fy[:, :-1, :] += ahead[:, 1:, :] - ahead[:, :-1, :]
+        dahead_fy[:, :-1, :] += ma_ahead[:, 1:, :] - ma_ahead[:, :-1, :]
+
         # Handle the stationary case
         if fwd_model.fl_model.regime == FlowRegime.STATIONARY:
             dhead_fy[:, :-1, :1] += (
@@ -357,15 +369,16 @@ def _get_perm_gradient_from_diffusivity_eq(
 
         # Bheadkward scheme
         dhead_by = np.zeros(shape)
-        dhead_by[:, 1:, :-1] += (
+        dhead_by[:, 1:, 1:] += (
             crank_flow * (head[:, :-1, 1:] - head[:, 1:, 1:])
             + (1.0 - crank_flow) * (head[:, :-1, :-1] - head[:, 1:, :-1])
         ) * dxi_harmonic_mean(permeability[:, 1:], permeability[:, :-1])[
             :, :, np.newaxis
         ]
         dahead_by = np.zeros(shape)
-        dahead_by[:, 1:, :] += ahead[:, :-1, :] - ahead[:, 1:, :]
+        dahead_by[:, 1:, :] += ma_ahead[:, :-1, :] - ma_ahead[:, 1:, :]
         # Handle the stationary case
+
         if fwd_model.fl_model.regime == FlowRegime.STATIONARY:
             dhead_by[:, 1:, :1] += (
                 (head[:, :-1, :1] - head[:, 1:, :1])