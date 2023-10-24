dcda31a39d1d4a15df3fab49579ddb14d4bc69f2
antoinecollet5 <antoine.collet5@gmail.com>
2023-10-24 12:34:57 +0200

    DOC: update lbfgsb docstring

--- pyrtid/inverse/lbfgsb.py
+++ pyrtid/inverse/lbfgsb.py
@@ -1,26 +1,33 @@
 """
-Python implementation of the lbfgsb code 778 in fortran.
+@author: Antoine COLLET.
 
-Author: Antoine COLLET.
+This code is a python port of the famous implementation of Limited-memory
+Broyden-Fletcher-Goldfarb-Shanno (L-BFGS), algorithm 778 written in Fortran [2,3]
+(last update in 2011).
+Note that this is not a wrapper such as minimize in scipy but a complete
+reimplementation (pure python).
+The original code can be found here: https://dl.acm.org/doi/10.1145/279232.279236
 
-Note: this is a manual reimplementation using the api of scipy. It should produce
-the same results than the original implementation. Python being slow, we tried to
-remove the for loops as much as possible and use python functions rather than direct
-accesses to linpack or minpack.
+The aim of this reimplementation was threefold. First, familiarize ourselves with
+the code, its logic and inner optimizations. Second, gain access to certain
+parameters that are hard-coded in the Fortran code and cannot be modified. Third,
+implement additional functionalities that require significant modification of
+the code core.
 
 Additional features
 --------------------
-Explain about the experimental feature. point to the doc of the main routine.
+Explain about objective function update on the fly.
+TODO: point to the doc of the main routine.
 
 References
 ----------
-* R. H. Byrd, P. Lu and J. Nocedal. A Limited Memory Algorithm for Bound
+[1] R. H. Byrd, P. Lu and J. Nocedal. A Limited Memory Algorithm for Bound
     Constrained Optimization, (1995), SIAM Journal on Scientific and
     Statistical Computing, 16, 5, pp. 1190-1208.
-* C. Zhu, R. H. Byrd and J. Nocedal. L-BFGS-B: Algorithm 778: L-BFGS-B,
+[2] C. Zhu, R. H. Byrd and J. Nocedal. L-BFGS-B: Algorithm 778: L-BFGS-B,
     FORTRAN routines for large scale bound constrained optimization (1997),
     ACM Transactions on Mathematical Software, 23, 4, pp. 550 - 560.
-* J.L. Morales and J. Nocedal. L-BFGS-B: Remark on Algorithm 778: L-BFGS-B,
+[3] J.L. Morales and J. Nocedal. L-BFGS-B: Remark on Algorithm 778: L-BFGS-B,
     FORTRAN routines for large scale bound constrained optimization (2011),
     ACM Transactions on Mathematical Software, 38, 1.
 """
