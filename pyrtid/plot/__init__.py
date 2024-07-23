4778c2cbbd23991b59709f5a63d845826e3140a0
antoinecollet5 <antoine.collet5@gmail.com>
2024-07-23 14:55:30 +0200

    ENH: add default configuration for the plots

--- pyrtid/plot/__init__.py
+++ pyrtid/plot/__init__.py
@@ -14,11 +14,13 @@ Functions to plot inversion results.
 
 """
 
+from pyrtid.plot.config import apply_default_rc_params
 from pyrtid.plot.plt_gradient import plot_2d_grad_res_adj_vs_fd
 from pyrtid.plot.plt_obs_vs_simu import plot_observed_vs_simulated
 from pyrtid.plot.plt_percentiles import plot_percentiles
 
 __all__ = [
+    "apply_default_rc_params",
     "plot_observed_vs_simulated",
     "plot_2d_grad_res_adj_vs_fd",
     "plot_percentiles",
---
+++ pyrtid/plot/config.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+
+def apply_default_rc_params() -> None:
+    """Default parameters to obtain nice plots."""
+    plt.plot()
+    plt.close()  # required for the plot to update
+    plt.rcParams.update(
+        {
+            "font.family": "sans-serif",
+            "font.sans-serif": ["Helvetica", "DejaVu Sans"],
+            "font.size": 16,
+            "text.usetex": False,
+            "savefig.format": "svg",
+            "svg.fonttype": "none",  # to store text as text, not as path
+            "savefig.facecolor": "w",
+            "savefig.edgecolor": "k",
+            "savefig.dpi": 300,
+            "figure.constrained_layout.use": True,
+            "figure.facecolor": "w",
+            "axes.titleweight": "bold",
+            "axes.labelweight": "bold",  # weight of the x and y labels
+            "figure.titleweight": "bold",
+            "axes.titlesize": 18,
+            "figure.titlesize": 22,
+            "animation.frame_format": "svg",
+        }
+    )
